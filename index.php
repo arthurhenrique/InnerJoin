e445029ecdcee7c95d3fe051ce0c2ad6aeee2fe9
Arthur Henrique <arthur393@users.noreply.github.com>
2014-11-29 18:36:36 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <h3>
-<table>
-<tr>
-<td>
-<a href='banco.php'> criar banco</a> <br></td>
-<td>
-<a href='insere_funcionario.php'> cadastra funcionario</a> <br></td>
-<td>
-<a href='insere_telefone.php'> cadastra telefone</a> <br></td>
-
-
-</tr>
-</table>
+	<table>
+		<tr>
+			<td><a href='banco.php'> criar banco</a> <br></td>
+			<td><a href='insere_funcionario.php'> cadastra funcionario</a> <br></td>
+			<td><a href='insere_telefone.php'> cadastra telefone</a> <br></td>
+		</tr>
+	</table>
 </h3>
 <?php
 
@@ -26,7 +21,7 @@ include("config.php");
 	while ($l=mysql_fetch_assoc($query)) {
 		echo "Nome: " . $l['DS_NOME']. "<br />";
 		echo "Telefone: " . $l['DS_TELEFONE'] . "<br />";
-		echo "Função: " . $l['DS_FUNCAO']. "<br/>";
+		echo "FunÃ§Ã£o: " . $l['DS_FUNCAO']. "<br/>";
 		echo "CPF:" . $l['NM_CPF']. "<br/>";
 		echo '<hr />';
 	}	
